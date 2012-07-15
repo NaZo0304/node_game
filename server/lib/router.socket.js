2ef2684fa1ec539c1aaf9c8b8ac5716d034b2ab7
Satoru Fukuda <harapeco39@gmail.com>
2012-07-15 23:30:01 +0900

    modify router.socket.js

--- server/lib/router.socket.js
+++ server/lib/router.socket.js
@@ -7,58 +7,58 @@
 *@licenseMIT
 *@respect:https://github.com/myatsumoto/socket.manager
 */
-varSocketRouter=module.exports={
-	set:function(controller,method,io,socket,dir){
-		socket.on(controller+'.'+method,function(data){
-			varcallback=SocketRouter.load(controller,method,dir);
-			callback(io,socket,data);
+var SocketRouter = module.exports = {
+	set: function(controller, method, io, socket, dir){
+		socket.on(controller + '.' + method,function(data){
+			var callback = SocketRouter.load(controller, method, dir);
+			callback(io, socket, data);
 		});
 	},
-	map:function(map,io,socket,dir){
-		for(varcontrollerinmap){
-			varmethod=map[controller];
-			if(method==='crud'){
-				SocketRouter.set(controller,'create',io,socket,dir);
-				SocketRouter.set(controller,'read',io,socket,dir);
-				SocketRouter.set(controller,'update',io,socket,dir);
-				SocketRouter.set(controller,'destroy',io,socket,dir);
+	map: function(map, io, socket, dir){
+		for(var controller in map){
+			var method = map[controller];
+			if(method === 'crud'){
+				SocketRouter.set(controller, 'create', io, socket, dir);
+				SocketRouter.set(controller, 'read', io, socket, dir);
+				SocketRouter.set(controller, 'update', io, socket, dir);
+				SocketRouter.set(controller, 'destroy', io, socket, dir);
 			}else{
-				SocketRouter.set(controller,method,io,socket,dir);
+				SocketRouter.set(controller, method, io, socket, dir);
 			}
 		}
 	},
-	load:function(controller,method,dir){
-		varfilepath=dir+'/'+controller;
-		returnmethod?require(filepath)[method]:require(filepath);
+	load: function(controller, method, dir){
+		var filepath = dir+'/'+controller;
+		return method ? require(filepath)[method] : require(filepath);
 	},
-	listen:function(app,map,dir,sessionStore){
-		vario=require('socket.io').listen(app);
+	listen: function(app, map, dir, sessionStore){
+		var io=require('socket.io').listen(app);
 		io.configure(function(){
-			io.set('authorization',function(handshakeData,callback){
-				varconnect=require('connect'),
-					parseCookie=connect.utils.parseCookie,
-					Session=connect.middleware.session.Session,
-					cookie=handshakeData.headers.cookie;
+			io.set('authorization',function(handshakeData, callback){
+				var connect = require('connect'),
+					parseCookie = connect.utils.parseCookie,
+					Session = connect.middleware.session.Session,
+					cookie = handshakeData.headers.cookie;
 				if(cookie){
-					handshakeData.cookie=cookie;
-					handshakeData.sessionId=parseCookie(cookie)['connect.sid'];
-					handshakeData.session=sessionStore;
-					sessionStore.get(handshakeData.sessionId,function(err,session){
+					handshakeData.cookie = cookie;
+					handshakeData.sessionId = parseCookie(cookie)['connect.sid'];
+					handshakeData.session = sessionStore;
+					sessionStore.get(handshakeData.sessionId, function(err, session){
 						if(err){
-							callback(err.message,false);
+							callback(err.message, false);
 						}else{
-							handshakeData.session=newSession(handshakeData,session);
-							callback(null,true);
+							handshakeData.session = new Session(handshakeData, session);
+							callback(null, true);
 						}
 					});
 				}else{
-					returncallback('Cookieisnotfound.',false);
+					return callback('Cookie is not found.', false);
 				}
 				callback(null,true);
 			});
 		});
-		io.sockets.on('connection',function(socket){
-			SocketRouter.map(map,io,socket,dir);
+		io.sockets.on('connection', function(socket){
+			SocketRouter.map(map, io, socket, dir);
 		});
 	}
 };
