0f798e9b24ffb78bdea7ca52fa79ebf6bddc1d4f
Tetsuya Ohta <ohta@raw-hide.co.jp>
2012-07-15 18:01:20 +0900

    redis

--- server/model/skill.js
+++ server/model/skill.js
@@ -5,12 +5,12 @@ var db = require('mongoose');
 db.connect(config.db.development.host + config.db.development.name);
 var Schema = db.Schema;
 
-var Skill = module.exports = db.model('players',
-	new Schema( {
-		name : String,
-		x : Number,
-		y : Number,
-		hp : Number,
-		mp : Number,
-	})
+var Skill = module.exports = db.model('skills',
+    new Schema( {
+	name : String,
+	x : Number,
+	y : Number,
+	hp : Number,
+	mp : Number,
+    })
 );
