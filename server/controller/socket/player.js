afa8fc5f4a166c50f0df24a65965d235a95528f7
Tetsuya Ohta <ohta@raw-hide.co.jp>
2012-07-16 15:30:24 +0900

    bnlodacaxstr

--- server/controller/socket/player.js
+++ server/controller/socket/player.js
@@ -22,8 +22,8 @@ var Player = module.exports = {
 		p.save();
 		p.c_id = p._id
 		p.id = p._id
-		console.log(p);
-		socket.emit(data.clientEvent, p);
+		console.log(data.clientEvent);
+		socket.broadcast.emit(data.clientEvent, p);
 	},
 	update: function(io,socket,data){
 		  console.log(data);
@@ -35,7 +35,7 @@ var Player = module.exports = {
 	 	//current_userを返却＆checkする
 		attr = { x: item.x, y: item.y, HP: item.HP, MP: item.MP, id: data.id, c_id: data.id, _id: data.id }
 
-		socket.emit(data.clientEvent, attr);
+		socket.broadcast.emit(data.clientEvent, attr);
 	},
 	read: function(io, socket, data){
 		 socket.emit(data.clientEvent, data);
