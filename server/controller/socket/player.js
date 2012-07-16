9441057a530aae054650659e30db4c55df50c45b
Tetsuya Ohta <ohta@raw-hide.co.jp>
2012-07-16 15:22:38 +0900

    hoge

--- server/controller/socket/player.js
+++ server/controller/socket/player.js
@@ -33,6 +33,7 @@ var Player = module.exports = {
 		attr = { x: item.x, y: item.y, HP: item.HP, MP: item.MP }
 		model.update({_id: data.id }, attr, function(err){});
 	 	//current_userを返却＆checkする
+		attr = { x: item.x, y: item.y, HP: item.HP, MP: item.MP, id: data.id, c_id: data.id, _id: data.id }
 
 		socket.emit(data.clientEvent, attr);
 	},
