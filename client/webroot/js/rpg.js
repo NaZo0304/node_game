39d7f1f269c443ea486b2d27eb481eb50ff6f8c0
masato kawakami <mkawakami0304@gmail.com>
2012-07-16 17:03:42 +0900

    rpg.ejs include js

--- client/webroot/js/rpg.js
+++ client/webroot/js/rpg.js
@@ -47,9 +47,6 @@ window.onload = function() {
 
         var image = new Surface(96, 128);
         image.draw(game.assets[images['chara']], 0, 0, 96, 128, 0, 0, 96, 128);
-        var playerModel = null;
-        /*
-
         var playerModel = new PlayerModel({ name: "player" });
         playerModel.player.image = image;
 
@@ -97,7 +94,7 @@ window.onload = function() {
                 }
             }
         });
-*/
+
         var stage = new Group();
         stage.addChild(map);
         game.rootScene.addChild(stage);
@@ -134,53 +131,6 @@ window.onload = function() {
           msg = $("#login-text").val();
           $("#login-text").val("");
           playerModel = new PlayerModel({ name: msg });
-          playerModel.player.image = image;
-
-          playerModel.player.isMoving = false;
-          playerModel.player.direction = 0;
-          playerModel.player.walk = 1;
-          playerModel.player.addEventListener('enterframe', function() {
-              this.frame = this.direction * 3 + this.walk;
-              if (this.isMoving) {
-                  this.moveBy(this.vx, this.vy);
-
-                  if (!(game.frame % 3)) {
-                      this.walk++;
-                      this.walk %= 3;
-                  }
-                  if ((this.vx && (this.x-8) % 16 == 0) || (this.vy && this.y % 16 == 0)) {
-                      this.isMoving = false;
-                      this.walk = 1;
-                  }
-                  playerModel.set("x",this.x);
-                  playerModel.set("y",this.y);
-                  playerModel.save();
-              } else {
-                  this.vx = this.vy = 0;
-                  if (game.input.left) {
-                      this.direction = 1;
-                      this.vx = -4;
-                  } else if (game.input.right) {
-                      this.direction = 2;
-                      this.vx = 4;
-                  } else if (game.input.up) {
-                      this.direction = 3;
-                      this.vy = -4;
-                  } else if (game.input.down) {
-                      this.direction = 0;
-                      this.vy = 4;
-                  }
-                  if (this.vx || this.vy) {
-                      var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * 16 : 0) + 16;
-                      var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * 16 : 0) + 16;
-                      if (0 <= x && x < map.width && 0 <= y && y < map.height && !map.hitTest(x, y)) {
-                          this.isMoving = true;
-                          arguments.callee.call(this);
-                      }
-                  }
-              }
-          });
-          stagePl.addChild(otherModel.player);
           if (msg.length > 0) {
              playerModel.save();
              game.rootScene.removeChild(input);
