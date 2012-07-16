a0fb75e8058ae1784fcfc643b0f27d250462e55b
masato kawakami <mkawakami0304@gmail.com>
2012-07-16 17:57:29 +0900

    rpg.ejs include js

--- client/webroot/js/rpg.js
+++ client/webroot/js/rpg.js
@@ -129,7 +129,7 @@ window.onload = function() {
         	}
 
         	console.log(player);
-            socket.on('player/' + msg.id + '.update', function(msg) {
+            socket.on('player.update', function(msg) {
             	console.log('player/' + msg.id + '.update :');
             	console.log(msg);
             });
